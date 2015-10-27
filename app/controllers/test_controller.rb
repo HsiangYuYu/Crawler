cd1d9fb056b432e6ca1eb879b29cb842dbb5cf81
hsiangyuyu <jumpingjump16@gmail.com>
2015-10-27 11:40:36 +0000

    Fix switchBar with seletecd button disabled

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -15,17 +15,22 @@ class TestController < ApplicationController
         case @view.to_i
         when 1
             url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=rnk/dc&page=#{@page}")   #精準度排列
+            @color1 = "#DEDEDE"
         when 2 
             url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/ac&page=#{@page}")  #升序排列
+            @color2 = "#DEDEDE"
         when 3
             url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/dc&page=#{@page}")  #降序排列
+            @color3 = "#DEDEDE"
         when 4
             url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=new/dc&page=#{@page}") #新上市
+            @color4 = "#DEDEDE"
         end
         @contents = JSON.parse(url.read)
         @data = @contents['prods']
         @totalPage = @contents['totalPage']
         
+
         respond_to do |format|
             format.js
         end
