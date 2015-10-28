98523ff08db341f1ef74f75bd8804391d05257e2
hsiangyuyu <jumpingjump16@gmail.com>
2015-10-28 10:22:44 +0000

    Adding Shopping Model

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -9,21 +9,21 @@ class TestController < ApplicationController
     
     def search
         @original_name = params[:search]
-        @name =  ERB::Util.url_encode(params[:search]) #與javascript的encodeURIComponent()相同
-        @page = params[:page]
-        @view = params[:view]
+        @name =  ERB::Util.url_encode(params[:search]) #與javascript的encodeURIComponent()相同,可將中文轉成網址編碼
+        @page = params[:page] #第幾頁
+        @view = params[:view] #要使用哪種view
         case @view.to_i
-        when 1
-            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=rnk/dc&page=#{@page}")   #精準度排列
+          when 1 #精準度排列
+            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=rnk/dc&page=#{@page}")   
             @color1 = "#DEDEDE"
-        when 2 
-            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/ac&page=#{@page}")  #升序排列
+          when 2 #升序排列
+            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/ac&page=#{@page}")  
             @color2 = "#DEDEDE"
-        when 3
-            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/dc&page=#{@page}")  #降序排列
+          when 3 #降序排列
+            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=prc/dc&page=#{@page}")  
             @color3 = "#DEDEDE"
-        when 4
-            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=new/dc&page=#{@page}") #新上市
+          when 4 #新上市
+            url = URI("http://ecshweb.pchome.com.tw/search/v3.3/all/results?q=#{@name}&sort=new/dc&page=#{@page}") 
             @color4 = "#DEDEDE"
         end
         @contents = JSON.parse(url.read)
@@ -34,5 +34,18 @@ class TestController < ApplicationController
         respond_to do |format|
             format.js
         end
+    end
+    
+    def create
+      @i = params[:i]
+      if(!Cart.exists?(name: params[:name])) #如果資料庫沒有那筆名子
+          Cart.create(name: params[:name] , price: params[:price])
+          @check_in_cart = false
+      else
+          @check_in_cart = true
+      end
+          respond_to do |format|
+            format.js
+          end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'test#index'
   get 'test/index' => 'test#index'
   get 'test/search' => 'test#search'
+  get 'test/create' => 'test#create'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
